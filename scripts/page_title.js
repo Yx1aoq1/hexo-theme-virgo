4c4eb27e2e5bd353ca334bc08d3f5ca3c8b7a4ce
Yx1aoq1 <704315493@qq.com>
2020-11-20 22:27:40 +0800

    feat: 样式修改

--- scripts/page_title.js
+++ scripts/page_title.js
@@ -8,7 +8,7 @@ hexo.extend.helper.register('page_title', function () {
   var title = this.page.title ? this.page.title : this.config.title;
 
   if (this.is_archive()) {
-    title = this.__('nav.articles');
+    title = this.__('menu.articles');
 
     if (this.is_month()) {
       title += ': ' + this.page.year + '/' + this.page.month;
@@ -16,9 +16,9 @@ hexo.extend.helper.register('page_title', function () {
       title += ': ' + this.page.year;
     }
   } else if (this.is_category()) {
-    title = this.__('nav.category') + ': ' + this.page.category;
+    title = this.__('menu.category') + ': ' + this.page.category;
   } else if (this.is_tag()) {
-    title = this.__('nav.tag') + ': ' + this.page.tag;
+    title = this.__('menu.tag') + ': ' + this.page.tag;
   }
 
   return title;
